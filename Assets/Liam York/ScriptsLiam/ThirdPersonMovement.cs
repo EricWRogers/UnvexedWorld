b7b3d93c19ecf95d40c86fa54fe94fadb9269433
Liam York <yorkliam08@gmail.com>
2024-08-31 13:54:12 -0500

    Added a jump to the player character and a tag for ground

--- Assets/Liam York/ScriptsLiam/ThirdPersonMovement.cs
+++ Assets/Liam York/ScriptsLiam/ThirdPersonMovement.cs
@@ -6,17 +6,44 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    public float gravity = -3.5f;
+
     public float speed = 6f;
 
     public float turnSmoothTime = 0.1f;
 
+    public bool isGrounded;
+
+    public bool isJumping;
+
+    public int jumpCount = 0;
+
+    public int jumpMax = 2;
+
+    public float jumpForce = 5f;
+
     float turnSmoothVelocity;
+
+    Vector3 velocity;
 
     public Transform cam;
 
     // Update is called once per frame
     void Update()
     {
+        if (!isGrounded)
+        {
+            
+            if (jumpCount < jumpMax)
+            {
+                isJumping = false;
+            }
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+            
+        }
+        
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
@@ -34,6 +61,28 @@ public class ThirdPersonMovement : MonoBehaviour
 
         }
 
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || jumpCount < jumpMax))
+        {
+            isJumping = true;
+            jumpCount++;
+            isGrounded = false;
+            velocity.y = 0;
+            velocity.y += jumpForce;
+            controller.Move(velocity * Time.deltaTime);
 
+        }
+
+        
+
+
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.transform.CompareTag("Ground")&& !isGrounded)
+        {
+            isGrounded = true;
+            Debug.Log("Ouch");
+            jumpCount = 0;
+        }
     }
 }
