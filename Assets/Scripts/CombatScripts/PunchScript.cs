3024a3366fd1f5a9fbf6746a87f5f1528fa98107
Liam York <yorkliam08@gmail.com>
2024-10-04 14:51:45 -0500

    Particulars destroy themselves

--- Assets/Scripts/CombatScripts/PunchScript.cs
+++ Assets/Scripts/CombatScripts/PunchScript.cs
@@ -34,7 +34,7 @@ public class PunchScript : MonoBehaviour, IDamageDealer
         Debug.Log("Hit" + other.gameObject.name);
         if (other.gameObject.tag == "GroundEnemy" || other.gameObject.tag == "Enemy")
         {   
-            particle = Instantiate(ParticleManager.Instance.NoSpellImpact, transform.position, Quaternion.Euler(transform.rotation.x-90,transform.rotation.y,transform.rotation.z));
+            /*particle = */Instantiate(ParticleManager.Instance.NoSpellImpact, transform.position, Quaternion.Euler(transform.rotation.x-90,transform.rotation.y,transform.rotation.z));
             enemy = other.gameObject;
             if(gameObject.GetComponent<Spell>()?.lifeSteal == true)
             {
