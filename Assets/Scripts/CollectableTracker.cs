c33f13764fc08e9e55d4150916ebcab593662878
Christian Wiggins <Christian.wiggins0@gmail.com>
2024-10-09 14:35:04 -0500

    Added collectable orb, script to pick it up, and script to track collected orbs

---
+++ Assets/Scripts/CollectableTracker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableTracker : MonoBehaviour
+{
+    //private CollectableOrb collectedOrbs;
+
+    public int collectedOrbs = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
