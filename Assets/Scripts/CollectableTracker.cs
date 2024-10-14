df50ab1829d5c229bfa7f6fec9ddbc52fd116d31
Christian Wiggins <Christian.wiggins0@gmail.com>
2024-10-13 21:11:55 -0500

    Collectible orb works, changed to hud prefab in ChristianCollectableTest scene
    The changes to the prefab show how many of 5 orbs have been collected when the player pauses, but I didn't want to change the prefab

--- Assets/Scripts/CollectableTracker.cs
+++ Assets/Scripts/CollectableTracker.cs
@@ -8,8 +8,16 @@ public class CollectableTracker : MonoBehaviour
 
     public int collectedOrbs = 0;
 
+    public GameObject collect1Block;
+    public GameObject collect2Block;
+    public GameObject collect3Block;
+    public GameObject collect4Block;
+    public GameObject collect5Block;
+
+    //private bool collected = true;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
     }
@@ -17,7 +25,27 @@ public class CollectableTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (collectedOrbs >= 1)
+        {
+            //collected = false;
+            collect1Block.SetActive(false);
+        }
+        if (collectedOrbs >=2)
+        {
+            collect2Block.SetActive(false);
+        }
+        if (collectedOrbs >=3)
+        {
+            collect3Block.SetActive(false);
+        }
+        if (collectedOrbs >=4)
+        {
+            collect4Block.SetActive(false);
+        }
+        if (collectedOrbs >=5)
+        {
+            collect5Block.SetActive(false);
+        }
     }
 
 }
