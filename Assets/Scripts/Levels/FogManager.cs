c32ea251656328482240d74ca931a8ce302c3011
Aiden Thacker <aidenthacker86@gmail.com>
2024-10-21 22:38:58 -0500

    Folder Reorganization Check

---
+++ Assets/Scripts/Levels/FogManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperPupSystems.Helper;
+
+public class FogManager : MonoBehaviour
+{
+    public GameObject group;
+    public GameObject fogArea;
+
+    [SerializeField]
+    private List<GameObject> groundEnemies = new List<GameObject>();
+
+    void Start()
+    {
+        groundEnemies.Clear();  // Clear the current list of enemies
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("GroundEnemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy != null && enemy.transform.IsChildOf(group.transform) && enemy.activeInHierarchy)  // Only enemies in the current area
+            {
+                groundEnemies.Add(enemy);
+            }
+        }
+
+        AddHealthListeners();
+    }
+
+    public void Update()
+    {
+        if (groundEnemies.Count == 0)
+        {
+            gameObject.SetActive(false);
+            fogArea.SetActive(false);
+        }
+    }
+
+    // Remove a specific enemy from the list when they die
+    public void RemoveEnemy(GameObject enemy)
+    {
+        if (groundEnemies.Contains(enemy))
+        {
+            groundEnemies.Remove(enemy);
+        }
+    }
+
+    // Add listeners to each enemy's health component
+    private void AddHealthListeners()
+    {
+        foreach (GameObject enemy in groundEnemies)
+        {
+            Health enemyHealth = enemy.GetComponent<Health>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.outOfHealth.AddListener(() => RemoveEnemy(enemy));
+            }
+        }
+    }
+}
