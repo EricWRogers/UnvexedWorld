2eee82fbd94f417550fd6b5edab098411b952520
Xander Adams <xanderadams555@gmail.com>
2025-02-06 15:51:42 -0600

    more groundwork

--- Assets/Scripts/CombatScripts/AttackUpdater.cs
+++ Assets/Scripts/CombatScripts/AttackUpdater.cs
@@ -4,7 +4,8 @@ public class AttackUpdater : MonoBehaviour
 {
     //elements
     //Direction??
-    
+    public SpellCraft.Aspect element = SpellCraft.Aspect.none;
+    public int aspect = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
