b274978e571d13f881199cceaf1a439d4e1f0e0b
Aiden Thacker <aidenthacker86@gmail.com>
2025-02-07 15:04:26 -0600

    Working on breakable objects for the white box
    Also added some Particles for the Mushroom for the second floor

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -7,12 +7,23 @@ public class BreakableObject : MonoBehaviour
     public GameObject unBrokenObject;
     public GameObject brokenObject;
 
+    [SerializeField]
+    private Rigidbody[] rb;
+
+    private float power = 5f;
+
     public string[] tagNames; //need to make this so that the we can check what can and cannot make the 
 
     private void Awake()
     {
         unBrokenObject.SetActive(true);
         brokenObject.SetActive(false);
+        rb = brokenObject.GetComponentsInChildren<Rigidbody>();
+    }
+
+    private void Start()
+    {
+
     }
 
     private void OnTriggerEnter(Collider col)
@@ -21,6 +32,17 @@ public class BreakableObject : MonoBehaviour
         {
             unBrokenObject.SetActive(false);
             brokenObject.SetActive(true);
+
+            if(rb != null)
+            {
+                foreach(Rigidbody rigidbodies in rb)
+                {
+                    float mag = rigidbodies.linearVelocity.magnitude;
+                    Vector3 dir = (transform.position - unBrokenObject.transform.position).normalized;
+                    rigidbodies.AddForce(dir * (power + mag), ForceMode.Impulse);
+                }
+            }
+
             Destroy(this, 1f);
         }
     }
