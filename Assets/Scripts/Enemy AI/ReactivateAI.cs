44d7ee756e8eeaecbcbd8e3c6bf56e07862d30f2
Aiden Thacker <aidenthacker86@gmail.com>
2025-03-05 15:27:45 -0600

    Got the Knockback in a pretty alright state

--- Assets/Scripts/Enemy AI/ReactivateAI.cs
+++ Assets/Scripts/Enemy AI/ReactivateAI.cs
@@ -3,12 +3,16 @@ using UnityEngine.AI;
 using SuperPupSystems.Helper;
 using SuperPupSystems.StateMachine;
 
+
 public class ReactivateAI : MonoBehaviour
 {
+    public float speedAmount = 0.5f;
     private GruntStateMachine gruntStateMachine;
     private NavMeshAgent agent;
     private Rigidbody rb;
+    private Rigidbody knockbackRB;
     private CapsuleCollider enemyCollider;
+    private Timer knockbackTimer;
     public GameObject enemy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,15 +33,25 @@ public class ReactivateAI : MonoBehaviour
             rb = enemy.GetComponent<Rigidbody>(); 
             enemyCollider = enemy.GetComponent<CapsuleCollider>();
         } 
+
+        knockbackRB = GetComponent<Rigidbody>();
+        knockbackTimer = GetComponent<Timer>();
     }
 
     void FixedUpdate()
     {
         transform.localEulerAngles = new Vector3(0f, transform.localEulerAngles.y, 0f);
+        Debug.Log("The Y velocity: " + Mathf.Abs(knockbackRB.linearVelocity.y));
     }
 
     public void SelfDestruct()
     {
+        if(Mathf.Abs(knockbackRB.linearVelocity.y) > speedAmount)
+        {
+            Debug.Log("Pile of Papers");
+            knockbackTimer.StartTimer(0.1f, false);
+            return;
+        }
         enemy.transform.parent = null;
         enemy.transform.parent = gruntStateMachine.ogParent.transform;
         enemyCollider.enabled = true;
