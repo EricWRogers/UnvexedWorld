60153054f209be6c01c5ffea5d3098ca313665e0
Liam York <yorkliam08@gmail.com>
2025-03-06 11:26:03 -0600

    fixed key orb problem and breaking objects now despair

--- Assets/Scripts/Collectable/KeyOrbCollect.cs
+++ Assets/Scripts/Collectable/KeyOrbCollect.cs
@@ -11,7 +11,7 @@ public class KeyOrbCollect : MonoBehaviour
     void Start()
     {
         popupText = FindFirstObjectByType<PopupText>();
-         gainedOrb = FindFirstObjectByType<keyOrbGainedScript>();
+        gainedOrb = FindFirstObjectByType<keyOrbGainedScript>();
     }
 
     // Update is called once per frame
