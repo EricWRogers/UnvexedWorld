6f6218b8af693dc6cab3955548b59b7a0e62e48f
Aiden Thacker <aidenthacker86@gmail.com>
2025-04-25 22:13:22 -0500

    Bout to blow this shit up

--- Assets/Scripts/Enemy AI/AnimationEventScripts/EnemyAnimationEvents.cs
+++ Assets/Scripts/Enemy AI/AnimationEventScripts/EnemyAnimationEvents.cs
@@ -8,7 +8,7 @@ public class EnemyAnimationEvents : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        health = GetComponentInParent<Health>();
+        
     }
 
     // Update is called once per frame
